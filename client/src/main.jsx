83fafd8a31765a98989fc33c1f4b9589840dca12
deepika dudala <deepika.dudala>
2024-01-05 11:22:31 +0530

    server commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { store } from "./app/store.js";
+import { store } from "./store/store.js";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv").config({ path: "server/.env" });
 const app = require("./app");
-
+const port = process.env.PORT || 4000;
 //database
 require("./database/connection");
 
 //server
-app.listen(process.env.PORT, () => {
-  console.log(`server listening at port ${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`server listening at port ${port}`);
 });
