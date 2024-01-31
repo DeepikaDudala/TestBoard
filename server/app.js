abb90226414ba163d3b31ecd5088598252501a86
deepika dudala <deepika.dudala>
2024-01-31 12:16:46 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -8,7 +8,13 @@ const testRoutes = require("./routes/testsRoutes");
 const resultsRoutes = require("./routes/resultsRoutes");
 const errorHandler = require("./middlewares/errorHandler");
 const cookieParser = require("cookie-parser");
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
